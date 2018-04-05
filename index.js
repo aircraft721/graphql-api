97c58fa6db452c1cdce77995b24d4b8b2475f72f
Dan Constantinescu <dan_constantinescu@outlook.com>
2018-04-05 16:50:57 +0300

    schema

--- index.js
+++ index.js
@@ -1,18 +1,140 @@
 const { GraphQLServer } = require('graphql-yoga');
 const fetch = require('node-fetch');
-const axios = require('axios');
+require('isomorphic-fetch');
 
 const typeDefs = `
     type Query {
-        getIndex(id: String!): Data
+        getIndex(id: String!): Data!
+    }
+
+    type InventoryStatusFlag {
+        active: Boolean!
+        out_of_stock: Boolean!
+        do_not_display: Boolean!
+        inactive: Boolean!
+        promotional: Boolean!
+        sold_out: Boolean!
+        coming_soon: Boolean!
+    }
+
+    type SkusCategories {
+        id: Int!
+        parent_id: Int!
+        url: String!
+    }
+
+    type Skus {
+        id: Int!
+        brand: String!
+        region: String!
+        language: String!
+        sku_type: String!
+        product_id: Int
+        product_code: String!
+        parent_category_id: Int!
+        categories: [SkusCategories]
+        display_name: String!
+        display_order: Int!
+        display_status: Int!
+        display_status_label: [String!]
+        hex_value: String!
+        inventory_status: Int!
+        inventory_status_label: String!
+        inventory_status_flag: InventoryStatusFlag!
+        label: String!
+        shade_name: String!
+        shade_description: String!
+        status_update_override: Boolean!
+        price: Int!
+        strikethrough_price: Int!
+        product_size: String!
+        unit_size: Int!
+    }
+
+    type CategoriesDescriptionHtml {
+        main: String!
+    }
+
+    type Categories {
+        description_html: CategoriesDescriptionHtml
+        name: String!
+        product_url: String!
+        display_order: Int!
+        id: Int!
+        parent_id: Int!
+        url: String!
+    }
+
+    type Videos {
+        name: String!
+        title: String!
+        file: String!
+    }
+
+    type Testimonial {
+        location: String
+        source: String
+        product: String
+    }
+
+    type PriceRange {
+        min: Int
+        max: Int
+    }
+
+    type Flag {
+        sized: Boolean
+        shaded: Boolean
+        sold_out: Boolean
+        coming_soon: Boolean
+        palette: Boolean
+        shoppable: Boolean
+        displayable: Boolean
+    }
+
+    type DescriptionHtml {
+        short: String
+        what_it_is: String
+        who_it_is_for: String
+        why_it_is_different: String
+        main: String
+    }
+
+    type Description {
+        short: String
+        what_it_is: String
+        who_it_is_for: String
+        why_it_is_different: String
+        main: String
     }
 
     type Source {
-        id: Int
-        brand: String
-        region: String
-        language: String
-        name: String
+        id: Int!
+        brand: String!
+        region: String!
+        language: String!
+        name: String!
+        display_name: String!
+        subheading: String
+        description: Description!
+        description_html: DescriptionHtml!
+        meta_description: String
+        usage: String
+        url: String!
+        default_category_id: Int
+        parent_category_id: Int
+        flag: Flag
+        keywords: String
+        meta_keywords: String
+        family_code: String
+        number_of_alt_images: Int
+        price_range: PriceRange
+        works_with_sku_ids: [Int!]!
+        testimonial: Testimonial
+        videos: [Videos!]!
+        tips: String
+        categories: [Categories]
+        skus: [Skus]
     }
 
     type Hits {
@@ -32,29 +154,25 @@ const typeDefs = `
 
 const resolvers = {
     Hits: {
-        _source: async parent => {
-            console.log('parent of source', parent);
-            const response = await fetch(parent._source);
-            return response;
+        _source: parent => {
+            return parent._source;
         }
     },
     Data: {
         hits: parent => {
-            const promises = parent.hits.map(async hits => {
-                const response = await fetch(hits);
-                console.log('hits', response);
-                return response;
+            const response = parent.hits.map(hits => {
+                return hits;
             });
-            return Promise.all(promises);
+            return response;
         }
     },
     Query: {
-        getIndex: async (_, { id }) => {
-            const response = await fetch(
+        getIndex: (_, { id }) => {
+            return fetch(
                 `https://qa.usva.api.elco.cloud/products-v0/${id}/_search/`
-            );
-            const x = await response.json();
-            return x.hits;
+            )
+                .then(res => res.json())
+                .then(res => res.hits);
         }
     }
 };
